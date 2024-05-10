95b045a36b27c1eb07264412e92424a692243dd0
Theripeer <cristianandrescv@gmail.com>
2024-05-10 18:13:29 -0400

    Estructura Stock

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Peticion recibida!');
+
+    res.send('Hola mundo con nodemon')
+})
+
+
+
+
+
+app.listen(4000, ()=>{
+    console.log('Servidor escuchando en el puerto:4000');
+})
